afe504b07b6d2e35ccd3e8729a01ee93d2c82251
Oscar <oscarj.rierav@gmail.com>
2025-04-21 20:23:58 -0400

    Commit this change

--- app/src/main/java/com/example/tiococo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/tiococo/ui/home/HomeActivity.kt
@@ -366,6 +366,7 @@ class HomeActivity : AppCompatActivity() {
             btnOption1.alpha = 0f
             btnOption2.alpha = 0f
             btnOption3.alpha = 0f
+            btnOption4.alpha = 0f
             btnOption1.animate().alpha(1f).setDuration(200).start()
             btnOption2.animate().alpha(1f).setDuration(200).start()
             btnOption3.animate().alpha(1f).setDuration(200).start()
