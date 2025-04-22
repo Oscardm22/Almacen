5da2b9aeb63199cd1f235d60b666b14bda2c394f
Oscar <oscarj.rierav@gmail.com>
2025-04-21 21:25:53 -0400

    Commit this change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        // Plugin necesario para Firebase
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
