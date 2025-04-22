11088b98c1a0f613317cce17e961b9862d16dc7f
Oscar <oscarj.rierav@gmail.com>
2025-04-21 21:11:54 -0400

    Commit this change

--- app/src/main/java/com/example/tiococo/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/tiococo/viewmodel/AuthViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.tiococo.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
